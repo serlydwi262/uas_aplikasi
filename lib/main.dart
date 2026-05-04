1f7ca467598122b5f214b63cd3722c9c01c5e879
serlydwi262 <serlydwirahayu03@gmail.com>
2026-05-04 23:32:21 +0700

    Commit 3:Setup Tema Aplikasi & Model Data

--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,56 @@ import 'dart:convert';
 void main() {
   runApp(const SpendWiseApp());
 }
+
+class SpendWiseApp extends StatelessWidget {
+  const SpendWiseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SpendWise',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class Transaksi {
+  final String id;
+  final String judul;
+  final double jumlah;
+  final DateTime tanggal;
+  final bool isPemasukan;
+
+  Transaksi({
+    required this.id,
+    required this.judul,
+    required this.jumlah,
+    required this.tanggal,
+    required this.isPemasukan,
+  });
+
+  Map<String, dynamic> topMap() {
+    return {
+      'id': id,
+      'judul': judul,
+      'jumlah': jumlah,
+      'tanggal': tanggal.toIso8601String(),
+      'isPemasukan': isPemasukan,
+    };
+  }
+
+  factory Transaksi.fromMap(Map<String, dynamic> map) {
+    return Transaksi(
+      id: map['id'],
+      judul: map['judul'],
+      jumlah: map['jumlah'],
+      tanggal: DateTime.parse(map['tanggal']),
+      isPemasukan: map['isPemasukan'],
+    );
+  }
+}
